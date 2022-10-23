37c8cb5cc179f09cf5ac2e990e0d2434a06bbc1b
Guang-Yu6 <everyting-day@foxmail.com>
2022-10-23 17:28:27 +0800

    创建dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
-    dialog
+    外层组件
   </div>
+  <h1>示例1</h1>
+  <hr>
+  <Dialog></Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components: {Dialog}
+}
+
+</script>
 
 <style lang='scss' scoped>
 
